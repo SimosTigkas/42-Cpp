358fc25629f464232aa6a283e8d02c5ba4e3f985
Simos Tigkas <stigkas@c2r6p9.hive.fi>
2024-06-19 13:46:33 +0300

    added a Parameterized Constructor

--- cpp00/ex02/Account.cpp
+++ cpp00/ex02/Account.cpp
@@ -6,7 +6,7 @@
 /*   By: stigkas <stigkas@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/19 12:19:55 by stigkas           #+#    #+#             */
-/*   Updated: 2024/06/19 13:36:44 by stigkas          ###   ########.fr       */
+/*   Updated: 2024/06/19 13:44:10 by stigkas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,6 +35,20 @@ Account::Account()
 	this->_accountIndex=get_index();
 	_nbAccounts++;
 	
+	_displayTimestamp();
+	std::cout << "index:" << this->_accountIndex << ";amount:"
+		<< this->_amount << ";created" << std::endl;
+}
+
+Account::Account(int initial_deposit)
+{
+	this->_amount = initial_deposit;
+	this->_nbDeposits = 0;
+	this->_nbWithdrawals = 0;
+	this->_accountIndex = get_index();
+	_totalAmount += this->_amount;
+	_nbAccounts++;
+
 	_displayTimestamp();
 	std::cout << "index:" << this->_accountIndex << ";amount:"
 		<< this->_amount << ";created" << std::endl;
