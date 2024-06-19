a1e01de650a858c4e2b1be9b821611436baec32d
Simos Tigkas <stigkas@c2r6p9.hive.fi>
2024-06-19 13:37:38 +0300

    constructed

--- cpp00/ex02/Account.cpp
+++ cpp00/ex02/Account.cpp
@@ -6,9 +6,36 @@
 /*   By: stigkas <stigkas@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/19 12:19:55 by stigkas           #+#    #+#             */
-/*   Updated: 2024/06/19 12:20:26 by stigkas          ###   ########.fr       */
+/*   Updated: 2024/06/19 13:36:44 by stigkas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Account.hpp"
+#include "Account.hpp"
+#include <sys/time.h>
+#include <iostream>
 
+int Account::_nbAccounts=0;
+int Account::_totalAmount=0;
+int Account::_totalNbDeposits=0;
+int Account::_totalNbWithdrawals=0;
+
+static int get_index()
+{
+	static int index;
+	index++;
+	return (index);	
+}
+
+Account::Account()
+{
+	this->_amount=0;
+	this->_nbDeposits=0;
+	this->_nbWithdrawals=0;
+	this->_accountIndex=get_index();
+	_nbAccounts++;
+	
+	_displayTimestamp();
+	std::cout << "index:" << this->_accountIndex << ";amount:"
+		<< this->_amount << ";created" << std::endl;
+}
