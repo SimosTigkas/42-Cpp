0098373a03186c167f3f33fcb764463f77fc1ae9
Simos Tigkas <stigkas@c2r6p9.hive.fi>
2024-06-19 13:49:23 +0300

    added a destructor

--- cpp00/ex02/Account.cpp
+++ cpp00/ex02/Account.cpp
@@ -6,7 +6,7 @@
 /*   By: stigkas <stigkas@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/19 12:19:55 by stigkas           #+#    #+#             */
-/*   Updated: 2024/06/19 13:44:10 by stigkas          ###   ########.fr       */
+/*   Updated: 2024/06/19 13:48:50 by stigkas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -52,4 +52,13 @@ Account::Account(int initial_deposit)
 	_displayTimestamp();
 	std::cout << "index:" << this->_accountIndex << ";amount:"
 		<< this->_amount << ";created" << std::endl;
+}
+
+Account::~Account()
+{
+	_nbAccounts--;
+	_totalAmount -= this->_amount;
+	_displayTimestamp();
+	std::cout << "index:" << this->_accountIndex << ";amount:"
+		<< this->_amount << ";closed" << std::endl;
 }
