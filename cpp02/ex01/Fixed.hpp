fdb0419388a4f4a3971f4eb71effc84def4c5ef4
Simos Tigkas <stigkas@c1r2p3.hive.fi>
2024-06-27 14:17:16 +0300

    lets do this

---
+++ cpp02/ex01/Fixed.hpp
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: stigkas <stigkas@student.hive.fi>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/27 13:13:29 by stigkas           #+#    #+#             */
+/*   Updated: 2024/06/27 14:15:20 by stigkas          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FIXED_HPP
+# define FIXED_HPP
+
+# include <iostream>
+# include <string>
+# include <cmath>
+
+class Fixed
+{
+    private:
+        int                 value;
+        static const int    bits=8;
+    public:
+        Fixed(const int num);
+        Fixed(const float nmro);
+        Fixed();
+        ~Fixed(void);
+        Fixed(const Fixed &new_obj);
+        Fixed &operator=(const Fixed &new_obj);
+        float   toFloat(void) const;
+        int     toInt(void) const;
+        int     getRawBits(void) const;
+        void    setRawBits(int const raw);
+};
+
+std::ostream &operator << (std::ostream &stream, const Fixed &nbr);
+
+#endif
---
+++ cpp02/ex01/main.cpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: stigkas <stigkas@student.hive.fi>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/27 13:13:15 by stigkas           #+#    #+#             */
+/*   Updated: 2024/06/27 13:27:25 by stigkas          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Fixed.hpp"
+
+int main(void) 
+{
+    Fixed a;
+    Fixed const b(10);
+    Fixed const c(42.42f);
+    Fixed const d(b);
+    a = Fixed(1234.4321f);
+    std::cout << "a is " << a << std::endl;
+    std::cout << "b is " << b << std::endl;
+    std::cout << "c is " << c << std::endl;
+    std::cout << "d is " << d << std::endl;
+    std::cout << "a is " << a.toInt() << " as integer" << std::endl;
+    std::cout << "b is " << b.toInt() << " as integer" << std::endl;
+    std::cout << "c is " << c.toInt() << " as integer" << std::endl;
+    std::cout << "d is " << d.toInt() << " as integer" << std::endl;
+    return 0;
+}
