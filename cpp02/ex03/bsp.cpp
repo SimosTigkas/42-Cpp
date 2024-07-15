67203054c52a8720752ac68c9434588c8ec438d4
Simos Tigkas <stigkas@c1r2p5.hive.fi>
2024-07-15 17:23:46 +0300

    cpp02 is finished

--- cpp02/ex03/bsp.cpp
+++ cpp02/ex03/bsp.cpp
@@ -6,7 +6,7 @@
 /*   By: stigkas <stigkas@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/28 14:02:50 by stigkas           #+#    #+#             */
-/*   Updated: 2024/07/15 17:19:08 by stigkas          ###   ########.fr       */
+/*   Updated: 2024/07/15 17:23:37 by stigkas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,11 +22,9 @@ static Fixed ft_abs(Fixed x)
 
 static Fixed area(Point const a, Point const b, Point const c)
 {
-    return (ft_abs(a.get_x() * (b.get_y() - c.get_y()) +
-                    b.get_x() * (c.get_y() - a.get_y()) +
-                    c.get_x() * (a.get_y() - b.get_y())) / Fixed(2));
+    return (ft_abs(a.get_x() * (b.get_y() - c.get_y()) + b.get_x() * (c.get_y() - a.get_y()) + c.get_x() * (a.get_y() - b.get_y())) / Fixed(2));
 }
-//(a.x * (b.y - c.y) + b.x * (c.y - a.y) + c.x * (a.y - b.y)) / 2.0)
+
 bool bsp( Point const a, Point const b, Point const c, Point const point)
 {
     Fixed   abc = area(a, b, c);
@@ -34,7 +32,7 @@ bool bsp( Point const a, Point const b, Point const c, Point const point)
     Fixed   pac = area(point, c, a);
     Fixed   pab = area(point, a, b);
 
-    std::cout << "partial triangle's area is: " << pbc + pac + pab << std::endl;
-    std::cout << "original triangle's area is: " << abc << std::endl;
+    std::cout << "Sum of the three triangles' areas is: " << pbc + pac + pab << std::endl;
+    std::cout << "Original triangle's area is: " << abc << std::endl;
     return (abc == (pbc + pac + pab));
 }
