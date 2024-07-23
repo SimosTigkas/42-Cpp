aacab4e55d78611da7fd01849f19c1b468e48eb9
Simos Tigkas <stigkas@c1r3p5.hive.fi>
2024-07-23 15:16:40 +0300

    will finish ex01 later

--- cpp04/ex01/Brain.hpp
+++ cpp04/ex01/Brain.hpp
@@ -6,19 +6,24 @@
 /*   By: stigkas <stigkas@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/23 14:05:33 by stigkas           #+#    #+#             */
-/*   Updated: 2024/07/23 14:07:18 by stigkas          ###   ########.fr       */
+/*   Updated: 2024/07/23 14:29:59 by stigkas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef BRAIN_HPP
 # define BRAIN_HPP
 
+#include <iostream>
+
 class Brain
 {
     private:
-        /* data */
+        std::string ideas[100];
     public:
-
+        Brain();
+        ~Brain();
+        Brain(const Brain &a_copy);
+        Brain &operator=(const Brain &og);
 };
 
 #endif
