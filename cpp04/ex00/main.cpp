b112cae1743221b9b88200c93896a7b5130a5ac3
Simos Tigkas <stigkas@c1r3p5.hive.fi>
2024-07-23 13:26:26 +0300

    ex00 is done

--- cpp04/ex00/main.cpp
+++ cpp04/ex00/main.cpp
@@ -6,34 +6,20 @@
 /*   By: stigkas <stigkas@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/23 08:03:38 by stigkas           #+#    #+#             */
-/*   Updated: 2024/07/23 13:19:11 by stigkas          ###   ########.fr       */
+/*   Updated: 2024/07/23 13:23:19 by stigkas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Dog.hpp"
-// #include "Cat.hpp"
+#include "Cat.hpp"
 
-#include "WrongCat.hpp"
+// #include "WrongCat.hpp"
 
 int main()
 {
-    // const Animal* meta = new Animal();
-    // const Animal* j = new Dog();
-    // const Animal* i = new Cat();
-    // std::cout << j->getType() << " " << std::endl;
-    // std::cout << i->getType() << " " << std::endl;
-    // i->makeSound(); //will output the cat sound!
-    // j->makeSound();
-    // meta->makeSound();
-
-    // delete meta;
-    // delete j;
-    // delete i;
-
-
-    const WrongAnimal* meta = new WrongAnimal();
+    const Animal* meta = new Animal();
     const Animal* j = new Dog();
-    const WrongAnimal* i = new WrongCat();
+    const Animal* i = new Cat();
     std::cout << j->getType() << " " << std::endl;
     std::cout << i->getType() << " " << std::endl;
     i->makeSound(); //will output the cat sound!
@@ -43,5 +29,19 @@ int main()
     delete meta;
     delete j;
     delete i;
+
+
+    // const WrongAnimal* meta = new WrongAnimal();
+    // const Animal* j = new Dog();
+    // const WrongAnimal* i = new WrongCat();
+    // std::cout << j->getType() << " " << std::endl;
+    // std::cout << i->getType() << " " << std::endl;
+    // i->makeSound(); //will output the cat sound!
+    // j->makeSound();
+    // meta->makeSound();
+
+    // delete meta;
+    // delete j;
+    // delete i;
     return 0;
 }
