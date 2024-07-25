5875b6971254fe18ccb20888e49661da70c96d1d
Simos Tigkas <suomikendam@gmail.com>
2024-07-25 14:08:39 +0300

    ex01 is almost finished

--- cpp04/ex01/main.cpp
+++ cpp04/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: stigkas <stigkas@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/23 08:03:38 by stigkas           #+#    #+#             */
-/*   Updated: 2024/07/23 13:51:40 by stigkas          ###   ########.fr       */
+/*   Updated: 2024/07/25 14:08:31 by stigkas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,30 +16,5 @@
 
 int main(void)
 {
-	const Animal	*meta = new Animal();
-	std::cout << "Animal type: " << meta->getType() << std::endl;
-	meta->makeSound();
-	delete meta;
-	std::cout << std::endl;
-
-	const Animal	*kissa = new Cat();
-	std::cout << "Cat type: " << kissa->getType() << std::endl;
-	kissa->makeSound();
-	delete kissa;
-
-	const Animal	*koira = new Dog();
-	std::cout << "Dog type: " << koira->getType() << std::endl;
-	koira->makeSound();
-	delete koira;
-
-	const WrongAnimal	*wrong_meta = new WrongAnimal();
-	std::cout << "Animal type: " << wrong_meta->getType() << std::endl;
-	wrong_meta->makeSound();
-	delete wrong_meta;
-
-	const WrongAnimal	*wrong_kissa = new WrongCat();
-	std::cout << "WrongCat type: " << wrong_kissa->getType() << std::endl;
-	wrong_kissa->makeSound();
-	delete wrong_kissa;
-    return 0;
+	
 }
