bbe91ed6c9779779658ce7b3aa58a39fefce7d5d
Simos Tigkas <stigkas@c2r6p1.hive.fi>
2024-10-17 14:19:52 +0300

    basic changes at hpp files

--- cpp05/ex03/Bureaucrat.hpp
+++ cpp05/ex03/Bureaucrat.hpp
@@ -1,12 +1,19 @@
 #ifndef BUREAUCRAT_HPP
 # define BUREAUCRAT_HPP
 
+#include <iostream>
 
+class Bureaucrat {
+    private:
+        const std::string   name;
+        int                 grade;
+    public:
+        ~Bureaucrat(void);
+		Bureaucrat(std::string name, int grade);
+		Bureaucrat(const Bureaucrat& rhs);
 
+		Bureaucrat& operator=(const Bureaucrat& rhs);
 
-
-
-
-
+};
 
 #endif
