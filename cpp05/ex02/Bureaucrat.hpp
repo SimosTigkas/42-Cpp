fc9b026131ac21eb3bc49d23455d8fd26142956f
Simos Tigkas <suomikendam@gmail.com>
2024-10-23 18:06:02 +0300

    updated with content of ex00

--- cpp05/ex02/Bureaucrat.hpp
+++ cpp05/ex02/Bureaucrat.hpp
@@ -1,19 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Bureaucrat.hpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: stigkas <stigkas@student.hive.fi>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/23 17:59:23 by stigkas           #+#    #+#             */
+/*   Updated: 2024/10/23 17:59:24 by stigkas          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef BUREAUCRAT_HPP
 # define BUREAUCRAT_HPP
 
 #include <iostream>
+#include <stdexcept>
 
 class Bureaucrat {
     private:
         const std::string   name;
         int                 grade;
     public:
+        Bureaucrat(void);
         ~Bureaucrat(void);
 		Bureaucrat(std::string name, int grade);
-		Bureaucrat(const Bureaucrat& rhs);
-
-		Bureaucrat& operator=(const Bureaucrat& rhs);
-
+        Bureaucrat(const Bureaucrat &a_copy);
+        Bureaucrat& operator=(const Bureaucrat& og);
+        class GradeTooLowException : public std::exception {
+            public:
+                const char *what() const throw();
+        };
+        class GradeTooHighException : public std::exception {
+            public:
+                const char *what() const throw();
+        };
+        unsigned int        getGrade() const;
+        const std::string   getName() const;
+        void                incrementGrade();
+        void                decrementGrade();
 };
+
+std::ostream &operator<<(std::ostream &out, const Bureaucrat &og);
 
 #endif
