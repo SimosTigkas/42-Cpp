f55fcbb709f8062e97688d5b3c7113f75cee7968
Simos Tigkas <suomikendam@gmail.com>
2024-10-23 18:52:12 +0300

    ex01 will be finished later today

--- cpp05/ex01/Form.cpp
+++ cpp05/ex01/Form.cpp
@@ -10,4 +10,13 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "From.hpp"
+#include "Form.hpp"
+
+Form::Form(std::string name, unsigned int sGrade, unsigned int eGrade): name(name), sGrade(sGrade), eGrade(eGrade)
+{
+    std::cout << "Form " << this->name <<" (with signing grade: " << sGrade << " and execution grade: "<< eGrade << ") has been created" << std::endl;
+    if (sGrade > 150 || eGrade > 150)
+        throw Form::GradeTooLowException();
+    else if (sGrade < 1 || eGrade < 1)
+        throw Form::GradeTooHighException();
+}
--- cpp05/ex01/Form.hpp
+++ cpp05/ex01/Form.hpp
@@ -15,8 +15,33 @@
 
 # include "Bureaucrat.hpp"
 
+class Form: public Bureaucrat
+{
+    private:
+        const std::string   name;
+        bool                isSigned;
+        const int           sGrade;
+        const int           eGrade;
+    public:
+        Form(void);
+        ~Form(void);
+        Form(std::string name, unsigned int sGrade, unsigned int eGrade);
+        Form& operator=(const Form& og);
+        Form(const Form &og);
+        const std::string	&getName() const;
+		const int			&getSGrade() const;
+		const int			&getEGrade() const;
+		bool				getIsSigned() const;
+        class GradeTooLowException : public std::exception {
+            public:
+                const char *what() const throw();
+        };
+        class GradeTooHighException : public std::exception {
+            public:
+                const char *what() const throw();
+        };
+};
 
-
-
+std::ostream &operator<<(std::ostream &out, const Form &og);
 
 #endif
