54687ce1304222569371e1221146c9c01cdebdbe
Simos Tigkas <suomikendam@gmail.com>
2024-10-29 13:24:19 +0200

    Pardoned

--- cpp05/ex02/PresidentialPardonForm.cpp
+++ cpp05/ex02/PresidentialPardonForm.cpp
@@ -11,3 +11,45 @@
 /* ************************************************************************** */
 
 #include "PresidentialPardonForm.hpp"
+
+PresidentialPardonForm::PresidentialPardonForm(void)
+{
+    std::cout << "PresidentialPardonForm " << this->target << " Default constructor has been called." <<std::endl;
+}
+
+PresidentialPardonForm::PresidentialPardonForm(std::string the_target): AForm("PresidentialPardonForm", 72, 45)
+{
+    target = the_target;
+}
+
+PresidentialPardonForm::PresidentialPardonForm(const PresidentialPardonForm &a_copy)
+{
+    this->target = a_copy.target;
+    std::cout << "PresidentialPardonForm " << this->target << " Copy Constructor has been called." <<std::endl;
+}
+
+PresidentialPardonForm::~PresidentialPardonForm(void)
+{
+    std::cout << "PresidentialPardonForm " << this->target << " has been destroyed." <<std::endl;
+}
+
+PresidentialPardonForm &PresidentialPardonForm::operator=(const PresidentialPardonForm &og)
+{
+    std::cout << "PresidentialPardonForm assignment operator has been called." << std::endl;
+    if (this != &og)
+    {
+        this->target = og.target;
+        return (*this);
+    }
+    return (*this);
+}
+
+void    PresidentialPardonForm::execute(Bureaucrat const& executor) const
+{
+    if (!this->getIsSigned())
+        throw FormIsNotSignedException();
+    if (executor.getGrade() <= this->getEGrade())
+        std::cout << target << " has been pardoned by Zaphod Beeblebrox." << std::endl;
+    else
+        throw GradeTooLowException();
+}
--- cpp05/ex02/PresidentialPardonForm.hpp
+++ cpp05/ex02/PresidentialPardonForm.hpp
@@ -13,6 +13,19 @@
 #ifndef PRESIDENTIALPARDONFORM_HPP
 # define PRESIDENTIALPARDONFORM_HPP
 
+# include "AForm.hpp"
 
+class PresidentialPardonForm: public AForm
+{
+    private:
+        std::string target;
+    public:
+        PresidentialPardonForm(void);
+        PresidentialPardonForm(std::string target);
+        ~PresidentialPardonForm();
+		PresidentialPardonForm(const PresidentialPardonForm &a_copy);
+		PresidentialPardonForm& operator=(const PresidentialPardonForm &og);
+        virtual void	execute(Bureaucrat const& executor) const override;
+};
 
 #endif
