be4c145712df72567c4df9447346083875b66576
Simos Tigkas <suomikendam@gmail.com>
2024-10-29 18:50:31 +0200

    let's do it

---
+++ cpp05/ex03/ShrubberyCreationForm.cpp
@@ -0,0 +1,66 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ShrubberyCreationForm.cpp                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: stigkas <stigkas@student.hive.fi>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/28 15:16:48 by stigkas           #+#    #+#             */
+/*   Updated: 2024/10/28 15:16:48 by stigkas          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ShrubberyCreationForm.hpp"
+
+ShrubberyCreationForm::ShrubberyCreationForm(void)
+{
+    std::cout << "ShrubberyCreationForm " << this->target << " Default constructor has been called." <<std::endl;
+}
+
+ShrubberyCreationForm::ShrubberyCreationForm(std::string the_target): AForm("ShrubberyCreationForm", 145, 137)
+{
+    target = the_target;
+}
+
+ShrubberyCreationForm::ShrubberyCreationForm(const ShrubberyCreationForm &a_copy): AForm(a_copy)
+{
+    this->target = a_copy.target;
+    std::cout << "ShrubberyCreationForm " << this->target << " Copy Constructor has been called." <<std::endl;
+}
+
+ShrubberyCreationForm::~ShrubberyCreationForm(void)
+{
+    std::cout << "ShrubberyCreationForm " << this->target << " has been destroyed." <<std::endl;
+}
+
+ShrubberyCreationForm &ShrubberyCreationForm::operator=(const ShrubberyCreationForm &og)
+{
+    std::cout << "ShrubberyCreationForm assignment operator has been called." << std::endl;
+    if (this != &og)
+    {
+        this->target = og.target;
+        return (*this);
+    }
+    return (*this);
+}
+
+void    ShrubberyCreationForm::execute(Bureaucrat const& executor) const
+{
+    std::string     tree;
+    std::ofstream   msg;
+
+    tree = "  #  \n ### \n#####\n  |  \n*****\n";
+    if (!this->getIsSigned())
+        throw FormIsNotSignedException();
+    if (executor.getGrade() <= this->getEGrade())
+    {
+        msg.open(target + "_shrubbery");
+        if (msg.is_open())
+        {
+            msg << tree;
+            msg.close();
+        }
+    }
+    else
+        throw GradeTooLowException();
+}
--- cpp05/ex03/main.cpp
+++ cpp05/ex03/main.cpp
@@ -5,9 +5,40 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: stigkas <stigkas@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/10/17 14:04:04 by stigkas           #+#    #+#             */
-/*   Updated: 2024/10/17 14:04:08 by stigkas          ###   ########.fr       */
+/*   Created: 2024/10/17 14:03:50 by stigkas           #+#    #+#             */
+/*   Updated: 2024/10/29 14:00:17 by stigkas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "ShrubberyCreationForm.hpp"
+#include "PresidentialPardonForm.hpp"
+#include "RobotomyRequestForm.hpp"
 #include "Bureaucrat.hpp"
+
+int main(void) 
+{
+    ShrubberyCreationForm blueberry("mustikka");
+    Bureaucrat Anton;
+    Bureaucrat Antti("Antti", 1);
+
+    Anton.signForm(blueberry);
+    Anton.executeForm(blueberry);
+    Antti.signForm(blueberry);
+    Antti.executeForm(blueberry);
+
+    PresidentialPardonForm zaphodPardon("Zaphod");
+    Anton.signForm(zaphodPardon);
+    Anton.executeForm(zaphodPardon);
+    Antti.signForm(zaphodPardon);
+    Antti.executeForm(zaphodPardon);
+
+    RobotomyRequestForm drill("driller");
+    Anton.signForm(drill);
+    Anton.executeForm(drill);
+    Antti.signForm(drill);
+    Antti.executeForm(drill);
+    Anton = Antti;
+    Anton.executeForm(drill);
+
+    return 0;
+}
