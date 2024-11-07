660760cbce2aaa58cee48dd55307e5cb2257fe03
Simos Tigkas <suomikendam@gmail.com>
2024-11-07 12:25:34 +0200

    added headers and generate()

--- cpp06/ex02/Base.cpp
+++ cpp06/ex02/Base.cpp
@@ -6,9 +6,34 @@
 /*   By: stigkas <stigkas@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/06 16:51:52 by stigkas           #+#    #+#             */
-/*   Updated: 2024/11/06 16:52:43 by stigkas          ###   ########.fr       */
+/*   Updated: 2024/11/07 12:24:41 by stigkas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Base.hpp"
 
+Base::~Base(void) {}
+
+Base *generate(void)
+{
+    int nmro;
+
+    std::srand(std::time(0));
+    nmro = rand() % 5;
+    if (nmro == 3)
+        return new A;
+    else if (nmro == 4)
+        return new B;
+    else
+        return new C;
+}
+
+void identify(Base *p)
+{
+    
+}
+
+void identify(Base &p)
+{
+    
+}
--- cpp06/ex02/Base.hpp
+++ cpp06/ex02/Base.hpp
@@ -6,17 +6,27 @@
 /*   By: stigkas <stigkas@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/06 16:51:06 by stigkas           #+#    #+#             */
-/*   Updated: 2024/11/06 16:52:43 by stigkas          ###   ########.fr       */
+/*   Updated: 2024/11/07 12:23:43 by stigkas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef BASE_HPP
 # define BASE_HPP
 
+# include <iostream>
+# include <ctime>
+# include "A.hpp"
+# include "B.hpp"
+# include "C.hpp"
+
 class Base
 {
-
+    public:
+        virtual ~Base(void);
 };
 
+Base *generate(void);
+void identify(Base *p);
+void identify(Base &p);
 
 #endif
