60a6106c41fe74a114e60ce2976c16de05745918
Simos Tigkas <suomikendam@gmail.com>
2024-11-08 13:20:18 +0200

    ex00 is done

--- cpp07/ex00/main.cpp
+++ cpp07/ex00/main.cpp
@@ -14,5 +14,22 @@
 
 int main(void)
 {
-    
+    std::string fname = "Simos";
+    std::string lname = "Tigkas";
+
+    std::cout << "First name: " << fname << " , Last name: " << lname << std::endl;
+    std::cout << "min(fname, lname) = " << ::min(fname, lname) << std::endl;
+	std::cout << "max(fname, lname) = " << ::max(fname, lname) << std::endl;
+    swap(fname, lname);
+    std::cout << "First name: " << fname << " , Last name: " << lname << std::endl;
+
+    int fps = 24;
+    int cinematic_fps = 50;
+
+    std::cout << "Fps: " << fps << ", Cinematic fps: " << cinematic_fps <<std::endl;
+    std::cout << "min(fps, cinematic_fps) = " << ::min(fps, cinematic_fps) << std::endl;
+	std::cout << "max(fps, cinematic_fps) = " << ::max(fps, cinematic_fps) << std::endl;
+    swap(fps, cinematic_fps);
+    std::cout << "Fps: " << fps << ", Cinematic fps: " << cinematic_fps <<std::endl;
+    return (0);
 }
