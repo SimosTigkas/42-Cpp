51dc99d2298f91aff2bc628190de6da7114dcd11
Simos Tigkas <suomikendam@gmail.com>
2024-11-11 14:03:13 +0200

    ex01 is done

--- cpp07/ex01/iter.hpp
+++ cpp07/ex01/iter.hpp
@@ -13,6 +13,29 @@
 #ifndef ITER_HPP
 # define ITER_HPP
 
+#include <iostream>
+#include <iomanip>
 
+template <typename T>
+void ft_print(T sth)
+{
+    if constexpr (std::is_floating_point<T>::value)
+        std::cout << std::fixed << std::setprecision(1) << sth << std::endl;
+    else
+        std::cout << sth << std::endl;
+}
+
+template <typename T>
+void iter(T *arr, size_t len, void (*f)(T))
+{
+    size_t i = 0;
+    if (len > sizeof(arr) || len < 1)
+        return ;
+    while (i < len)
+    {
+        f(arr[i]);
+        i++;
+    }
+}
 
 #endif
--- cpp07/ex01/main.cpp
+++ cpp07/ex01/main.cpp
@@ -14,5 +14,10 @@
 
 int main(void)
 {
-    
+    double      aperture[] = {1.4 , 2.0, 2.8, 4.0, 5.6, 8.0, 11.0, 16.0};
+    std::string camera[] = {"Sony", "Canon", "Fujifilm", "Nikon"};
+
+    iter(aperture, 8, &ft_print);
+    std::cout << std::endl;
+    iter(camera, 4, &ft_print); 
 }
