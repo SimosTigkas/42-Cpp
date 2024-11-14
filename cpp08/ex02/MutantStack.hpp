b9ecda416cd509ff05b562f31aba8bdffd172be7
Simos Tigkas <stigkas@c2r5p13.hive.fi>
2024-11-14 13:48:35 +0200

    ex02 is almost done

--- cpp08/ex02/MutantStack.hpp
+++ cpp08/ex02/MutantStack.hpp
@@ -6,15 +6,46 @@
 /*   By: stigkas <stigkas@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/14 12:36:56 by stigkas           #+#    #+#             */
-/*   Updated: 2024/11/14 12:38:30 by stigkas          ###   ########.fr       */
+/*   Updated: 2024/11/14 13:47:39 by stigkas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MUTANTSTACK_HPP
 # define MUTANTSTACK_HPP
 
+# include <iostream>
+# include <stack>
+# include <algorithm>
+# include <iterator>
+# include <list>
 
-
-
+template <typename T>
+class MutantStack: public std::stack<T>
+{
+    private:
+    public:
+        MutantStack(void);
+        ~MutantStack(void);
+        MutantStack(const MutantStack &a_copy);
+        MutantStack &operator=(const MutantStack &og);
+        typedef typename std::stack<T>::container_type::iterator iterator;
+        typedef typename std::stack<T>::container_type::const_iterator citerator;
+        iterator begin()
+        {
+            return (std::stack<T>::c.begin());
+        }
+        iterator end()
+        {
+            return (std::stack<T>::c.end());
+        }
+        citerator begin() const
+        {
+            return (std::stack<T>::c.begin());
+        }
+        citerator end() const
+        {
+            return (std::stack<T>::c.end());
+        }
+};
 
 #endif
