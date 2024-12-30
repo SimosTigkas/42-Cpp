9f6dba05f1cb98431fa9229499de926c06b8abce
Simos Tigkas <stigkas@c2r2p1.hive.fi>
2024-12-30 16:45:39 +0200

    cpp05 is done

--- cpp05/ex03/main.cpp
+++ cpp05/ex03/main.cpp
@@ -17,40 +17,114 @@ int main(void)
     std::cout << "\n\033[34mLET'S CREATE SOME FORMS!!\n\033[0m" <<std::endl;
 
     ShrubberyCreationForm blueberry("mustikka");
-    Bureaucrat anton("Anton", 7);
-    std::cout << anton << std::endl;
-    anton.signForm(blueberry);
-    anton.executeForm(blueberry);
-    Bureaucrat antti("Antti", 1);
-    std::cout << antti << std::endl;
-    antti.signForm(blueberry);
-    antti.executeForm(blueberry);
-    
-    std::cout << std::endl;
-    
-    Intern simos;
-    AForm* stigkas = simos.makeForm("RobotomyRequestForm", "drill");
-    anton.signForm(*stigkas);
-    anton.executeForm(*stigkas);
-    antti.signForm(*stigkas);
-    antti.executeForm(*stigkas);
-    anton = antti;
-    std::cout << anton << std::endl;
-    anton.executeForm(*stigkas);
-    
-    std::cout << std::endl;
-    
-    Intern safa;
-    AForm* stuna = safa.makeForm("PresidentialPardonForm", "Zaphod");
-    anton.signForm(*stuna);
-    anton.executeForm(*stuna);
-    antti.signForm(*stuna);
-    antti.executeForm(*stuna);
+    Bureaucrat* anton = nullptr;
+    Bureaucrat* antti = nullptr;
+    try
+    {
+        try
+        {
+            anton = new Bureaucrat("Anton", 35);
+            std::cout << *anton << "\n" << std::endl;
+        }
+        catch (const Bureaucrat::GradeTooHighException& e)
+        {
+            std::cerr << "\033[31mError creating Anton: " << e.what() << "\033[0m\n";
+        }
+        catch (const Bureaucrat::GradeTooLowException& e)
+        {
+            std::cerr << "\033[31mError creating Anton: " << e.what() << "\033[0m\n";
+        }
+        if (anton)
+        {
+            anton->signForm(blueberry);
+            anton->executeForm(blueberry);
+        }
 
-    if (stigkas)
-        delete stigkas;
-    if (stuna)
-        delete stuna;
+        try
+        {
+            antti = new Bureaucrat("Antti", 2);
+            std::cout << *antti << "\n" << std::endl;
+        }
+        catch (const Bureaucrat::GradeTooHighException& e)
+        {
+            std::cerr << "\033[31mError creating Antti: " << e.what() << "\033[0m\n";
+        }
+        catch (const Bureaucrat::GradeTooLowException& e)
+        {
+            std::cerr << "\033[31mError creating Antti: " << e.what() << "\033[0m\n";
+        }
+        if (antti)
+        {
+            antti->signForm(blueberry);
+            antti->executeForm(blueberry);
+        }
+        std::cout << std::endl;
+    
+        Intern simos;
+        AForm *stigkas = nullptr;
+        try
+        {
+            if (anton && antti)
+            {
+                stigkas = simos.makeForm("RobotomyRequestForm", "drill");
+                anton->signForm(*stigkas);
+                anton->executeForm(*stigkas);
+                antti->signForm(*stigkas);
+                antti->executeForm(*stigkas);
+                *anton = *antti;
+                std::cout << *anton << std::endl;
+                anton->executeForm(*stigkas);
+            }
+        }
+        catch (const Bureaucrat::GradeTooHighException& e)
+        {
+            std::cerr << "\033[31mError creating form: " << e.what() << "\033[0m\n";
+        }
+        catch (const Bureaucrat::GradeTooLowException& e)
+        {
+            std::cerr << "\033[31mError creating form: " << e.what() << "\033[0m\n";
+        }
+    
+        std::cout << std::endl;
+    
+        Intern safa;
+        AForm* stuna = nullptr;
+        try
+        {
+            if (anton && antti)
+            {
+                stuna = safa.makeForm("PresidentialPardonForm", "Zaphod");
+                anton->signForm(*stuna);
+                anton->executeForm(*stuna);
+                antti->signForm(*stuna);
+                antti->executeForm(*stuna);
+            }
+        }
+        catch (const Bureaucrat::GradeTooHighException& e)
+        {
+            std::cerr << "\033[31mError creating form: " << e.what() << "\033[0m\n";
+        }
+        catch (const Bureaucrat::GradeTooLowException& e)
+        {
+            std::cerr << "\033[31mError creating form: " << e.what() << "\033[0m\n";
+        }
+        if (stigkas)
+            delete stigkas;
+        if (stuna)
+            delete stuna;
+    }
+    catch (const Bureaucrat::GradeTooHighException& e)
+    {
+        std::cerr << "\033[31mError creating Antti: " << e.what() << "\033[0m\n";
+    }
+    catch (const Bureaucrat::GradeTooLowException& e)
+    {
+        std::cerr << "\033[31mError creating Antti: " << e.what() << "\033[0m\n";
+    }
+    if (anton)
+        delete anton;
+    if (antti)
+        delete antti;    
     std::cout << std::endl;
 
     return 0;
