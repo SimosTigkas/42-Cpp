cb17dd075d07ca1182256abcde204756ecf2245c
Simos Tigkas <suomikendam@gmail.com>
2025-01-21 14:52:47 +0200

    lets do it

--- cpp09/ex02/PmergeMe.hpp
+++ cpp09/ex02/PmergeMe.hpp
@@ -1,13 +1,34 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.cpp                                           :+:      :+:    :+:   */
+/*   PmergeMe.hpp                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: stigkas <stigkas@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/11/18 17:39:06 by stigkas           #+#    #+#             */
-/*   Updated: 2024/11/18 17:39:06 by stigkas          ###   ########.fr       */
+/*   Created: 2025/01/21 14:48:53 by stigkas           #+#    #+#             */
+/*   Updated: 2025/01/21 14:48:53 by stigkas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "PmergeMe.hpp"
+#ifndef PMERGEME_HPP
+# define PMERGEME_HPP
+
+# include <iostream>
+
+class PmergeMe
+{
+    private:
+
+    public:
+        PmergeMe(void);
+        PmergeMe(const std::string& input);
+        ~PmergeMe(void);
+        PmergeMe(const PmergeMe &a_copy);
+        PmergeMe &operator=(const PmergeMe &og);
+};
+
+
+
+
+
+#endif
